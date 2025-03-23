47bfdbb7861712fe6648a6633e4a9b209c3b1d74
sikigasa <km3t1114@yahoo.co.jp>
2025-03-23 23:23:00 +0900

    update: add entities

--- internal/domain/entities.go
+++ internal/domain/entities.go
@@ -1,1 +1,8 @@
 package domain
+
+type Task struct {
+	ID          int64  `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	IsEnd       bool   `json:"is_end"`
+}
