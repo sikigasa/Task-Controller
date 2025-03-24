a96ce87af95ecdaef479630c05bf1c05dbe38e26
sikigasa <km3t1114@yahoo.co.jp>
2025-03-24 21:35:53 +0900

    feat: createTask

--- internal/infra/task.go
+++ internal/infra/task.go
@@ -1,1 +1,57 @@
 package infra
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/sikigasa/task-controller/internal/domain"
+)
+
+type taskRepo struct {
+	db *sql.DB
+}
+
+type TaskRepo interface {
+	Create(ctx context.Context, arg domain.CreateTaskParam) error
+	Get(ctx context.Context, arg domain.GetTaskParam) (*domain.Task, error)
+
+	Delete(ctx context.Context, arg domain.DeleteTaskParam) error
+}
+
+func NewTaskRepo(db *sql.DB) TaskRepo {
+	return &taskRepo{db: db}
+}
+
+func (t *taskRepo) Create(ctx context.Context, arg domain.CreateTaskParam) error {
+	const query = `INSERT INTO task (title, description, authority) VALUES ($1,$2,$3)`
+
+	row := t.db.QueryRowContext(ctx, query, arg.Title, arg.Description, arg.IsEnd)
+
+	return row.Err()
+}
+
+func (t *taskRepo) Get(ctx context.Context, arg domain.GetTaskParam) (*domain.Task, error) {
+	const query = `SELECT task_id,project_id,authority FROM tasks WHERE task_id = $1`
+	row := t.db.QueryRowContext(ctx, query, arg.ID)
+	var task domain.Task
+	if err := row.Scan(&task.ID, &task.Title, &task.Description); err != nil {
+		return nil, err
+	}
+	return &task, nil
+}
+
+func (t *taskRepo) Delete(ctx context.Context, arg domain.DeleteTaskParam) error {
+	const query = `DELETE FROM tasks WHERE project_id = $1`
+	row, err := t.db.ExecContext(ctx, query, arg.ID)
+	if err != nil {
+		return err
+	}
+	count, err := row.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if count == 0 {
+		return sql.ErrNoRows
+	}
+	return nil
+}
--- internal/usecase/task.go
+++ internal/usecase/task.go
@@ -1,9 +1,31 @@
 package usecase
 
 import (
+	"context"
+
+	"github.com/sikigasa/task-controller/internal/infra"
 	task "github.com/sikigasa/task-controller/proto/v1"
 )
 
 type taskService struct {
 	task.UnimplementedTaskServiceServer
+	taskRepo infra.TaskRepo
+}
+
+func NewTaskService(taskRepo infra.TaskRepo) task.TaskServiceServer {
+	return &taskService{
+		taskRepo: taskRepo,
+	}
+}
+
+func (t *taskService) CreateTask(ctx context.Context, req *task.CreateTaskRequest) (*task.CreateTaskResponse, error) {
+	param := task.CreateTaskParam{
+		Title:       req.Title,
+		Description: req.Description,
+		IsEnd:       req.IsEnd,
+	}
+
+	return &task.CreateTaskResponse{
+		Id: "generated-task-id", // Replace with actual task ID
+	}, nil
 }
