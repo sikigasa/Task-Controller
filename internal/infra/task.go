090430cbcba8bc359b67940252779a750cd7ba52
sikigasa <km3t1114@yahoo.co.jp>
2025-03-28 03:37:55 +0900

    [WIP]feat: GetAll & Update

--- internal/infra/task.go
+++ internal/infra/task.go
@@ -14,7 +14,8 @@ type taskRepo struct {
 type TaskRepo interface {
 	Create(ctx context.Context, arg domain.CreateTaskParam) error
 	Get(ctx context.Context, arg domain.GetTaskParam) (*domain.Task, error)
-
+	GetAll(ctx context.Context, arg domain.GetAllTaskParam) ([]domain.Task, error)
+	Update(ctx context.Context, arg domain.UpdateTaskParam) error
 	Delete(ctx context.Context, arg domain.DeleteTaskParam) error
 }
 
@@ -38,6 +39,34 @@ func (t *taskRepo) Get(ctx context.Context, arg domain.GetTaskParam) (*domain.Ta
 		return nil, err
 	}
 	return &task, nil
+}
+
+func (t *taskRepo) GetAll(ctx context.Context, arg domain.GetAllTaskParam) ([]domain.Task, error) {
+	const query = `SELECT task_id,project_id,authority FROM tasks LIMIT $1 OFFSET $2`
+	rows, err := t.db.QueryContext(ctx, query, arg.Limit, arg.Offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var tasks []domain.Task
+	for rows.Next() {
+		var task domain.Task
+		if err := rows.Scan(&task.ID, &task.Title, &task.Description); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return tasks, nil
+
+}
+
+func (t *taskRepo) Update(ctx context.Context, arg domain.UpdateTaskParam) error {
+	const query = `UPDATE tasks SET title = $1, description = $2 WHERE task_id = $3`
+	row := t.db.QueryRowContext(ctx, query, arg.Title, arg.Description, arg.ID)
+	return row.Err()
 }
 
 func (t *taskRepo) Delete(ctx context.Context, arg domain.DeleteTaskParam) error {
