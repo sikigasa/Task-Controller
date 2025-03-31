7af53087aa5cb7f62f2b5795d962c89a857db670
sikigasa <km3t1114@yahoo.co.jp>
2025-03-31 23:28:07 +0900

    update: drop

--- db/migrations/000001_create_task_table.down.sql
+++ db/migrations/000001_create_task_table.down.sql
@@ -1,1 +1,5 @@
-DROP TABLE IF EXISTS task;
+DROP TRIGGER IF EXISTS set_updated_at ON "task";
+DROP FUNCTION IF EXISTS update_updated_at_column();
+DROP TABLE IF EXISTS "task_tag";
+DROP TABLE IF EXISTS "tag";
+DROP TABLE IF EXISTS "task";
