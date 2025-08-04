eaac58c4e0e26fc419559f3fb8d3a72f2b9ae05d
sikigasa <soninoo7mod@gmail.com>
2025-08-04 19:45:18 +0900

    todo: use test-container

--- internal/usecase/task_test.go
+++ internal/usecase/task_test.go
@@ -1,35 +1,35 @@
 package usecase
 
-import (
-	"context"
-	"testing"
+// import (
+// 	"context"
+// 	"testing"
 
-	task "github.com/sikigasa/task-controller/proto/v1"
-	"google.golang.org/protobuf/types/known/timestamppb"
-)
+// 	task "github.com/sikigasa/task-controller/proto/v1"
+// 	"google.golang.org/protobuf/types/known/timestamppb"
+// )
 
-func TestTask(t *testing.T) {
-	t.Run("CreateTask", testCreateTask)
-}
+// func TestTask(t *testing.T) {
+// 	t.Run("CreateTask", testCreateTask)
+// }
 
-func testCreateTask(t *testing.T) {
+// func testCreateTask(t *testing.T) {
 
-	t.Run("正常系", func(t *testing.T) {
+// 	t.Run("正常系", func(t *testing.T) {
 
-		req := &task.CreateTaskRequest{
-			Title:       "test",
-			Description: "test",
-			LimitedAt:   timestamppb.Now(),
-			TagIds:      []string{"tag1", "tag2"},
-		}
+// 		req := &task.CreateTaskRequest{
+// 			Title:       "test",
+// 			Description: "test",
+// 			LimitedAt:   timestamppb.Now(),
+// 			TagIds:      []string{"tag1", "tag2"},
+// 		}
 
-		res, err := taskService.CreateTask(context.Background(), req)
-		if err != nil {
-			t.Errorf("expected no error, got %v", err)
-		}
+// 		res, err := taskService.CreateTask(context.Background(), req)
+// 		if err != nil {
+// 			t.Errorf("expected no error, got %v", err)
+// 		}
 
-		if res == nil {
-			t.Errorf("expected response, got nil")
-		}
-	})
-}
+// 		if res == nil {
+// 			t.Errorf("expected response, got nil")
+// 		}
+// 	})
+// }
