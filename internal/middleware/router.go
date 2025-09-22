4b771322a4808095b7b5b370ed4c170c4078ae05
sikigasa <soninoo7mod@gmail.com>
2025-09-22 18:58:08 +0900

    fix: apply CORS middleware to task and tag service handlers

--- internal/middleware/router.go
+++ internal/middleware/router.go
@@ -29,10 +29,11 @@ func (r *router) SetupRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	taskPath, taskHandler := connect.NewTaskServiceHandler(r.taskService)
-	mux.Handle(taskPath, taskHandler)
+	mux.Handle(taskPath, CORSMiddleware(taskHandler))
+	mux.Handle("GET /tasks/", http.StripPrefix("/tasks", taskHandler))
 
 	tagPath, tagHandler := connect.NewTagServiceHandler(r.tagService)
-	mux.Handle(tagPath, tagHandler)
+	mux.Handle(tagPath, CORSMiddleware(tagHandler))
 
 	return mux
 }
